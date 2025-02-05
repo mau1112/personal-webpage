7809d6c6bd7c883169f4dc11b0088cef07d7bb3f
Mauricio Galeano <mauriciogaleano1112@gmail.com>
2025-02-05 17:52:01 -0500

    first working endpoint, added htmx

--- chuzos/views.py
+++ chuzos/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+import json
+
+from django.shortcuts import render, get_object_or_404
+
+
 from chuzos.models import ChuzoReview
 
 # Create your views here.
 
 def chuzos_list_view(request):
-    chuzos = ChuzoReview.objects.all()
-    pass
+    chuzos = ChuzoReview.objects.all().values()
+    chuzos = json.dumps(chuzos, default=str)
+    context = {'chuzos': chuzos}
+    return render(request, "chuzos/chuzos_list.html", context)
 
 def chuzos_detail_view(request, chuzo_id):
-    chuzo = ChuzoReview.objects.get(id=chuzo_id)
+    chuzo = get_object_or_404(ChuzoReview, id=chuzo_id)
     pass
--- mySite/settings.py
+++ mySite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mySite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
